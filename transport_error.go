d1b769201d8059681511a28a7b439b19b0ca2e47
Jeremy Price <donomii@gmail.com>
2025-12-10 19:57:22 +0900

    ugh

---
+++ transport_error.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net"
+	"net/url"
+	"syscall"
+)
+
+func isNetworkTransportError(err error) bool {
+	if err == nil {
+		return false
+	}
+	var urlErr *url.Error
+	if errors.As(err, &urlErr) {
+		return true
+	}
+	var netErr net.Error
+	if errors.As(err, &netErr) {
+		return true
+	}
+	if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+		return true
+	}
+	if errors.Is(err, syscall.ECONNRESET) || errors.Is(err, syscall.ECONNREFUSED) || errors.Is(err, syscall.ETIMEDOUT) {
+		return true
+	}
+	return false
+}
